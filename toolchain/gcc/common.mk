f9cb25df3dde798a0b02721cf43b33c42078a492
beermix <27rurk@gmail.com>
2020-09-16 16:58:40 +0300

    autocommit on change

--- toolchain/gcc/common.mk
+++ toolchain/gcc/common.mk
@@ -168,8 +168,6 @@ ifeq ($(CONFIG_TARGET_x86)$(CONFIG_USE_GLIBC)$(CONFIG_INSTALL_GCCGO),yyy)
   TARGET_CFLAGS+=-fno-split-stack
 endif
 
-# TARGET_CFLAGS := -O2 $(filter-out -fno-caller-saves -O%,$(TARGET_CFLAGS))
-
 GCC_MAKE:= \
 	export SHELL="$(BASH)"; \
 	$(MAKE) \
