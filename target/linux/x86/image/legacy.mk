16bd4b2da31f78aa737775477b75b80a16cdca0d
beermix <27rurk@gmail.com>
2020-09-16 16:35:41 +0300

    autocommit on change

--- target/linux/x86/image/legacy.mk
+++ target/linux/x86/image/legacy.mk
@@ -1,7 +1,6 @@
 define Device/generic
   DEVICE_TITLE := Generic x86/legacy
   DEVICE_PACKAGES += kmod-r8169
-	kmod-tg3 kmod-via-rhine kmod-via-velocity kmod-forcedeth
   GRUB2_VARIANT := legacy
 endef
 TARGET_DEVICES += generic
